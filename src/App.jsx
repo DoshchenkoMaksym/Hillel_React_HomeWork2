1bcfced36b900859314b2c7201e6f7aa6e029c03
MaksymDoshchenko <doschenko54@gmail.com>
2022-09-29 13:05:01 +0300

    hw2 done

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+import UserInfo from './UserInfo/UserInfo';
+function App() {
+  return (
+    <UserInfo />
+  );
+}
+
+export default App;
---
+++ src/UserInfo/UserInfo.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+class UserInfo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: 'Stepan',
+            age: 25,
+            isLogin: true,
+        };
+        this.handleChange = this.handleChange.bind(this);
+    };
+
+    handleChange() {
+        this.setState({
+            name: 'Mykola',
+            age: 30,
+            isLogin: !this.state.isLogin,
+        })
+    };
+
+    render() {
+        return (
+
+            <div>
+                {this.state.isLogin ?
+                    <p>Name: {this.state.name}, age: {this.state.age}</p> :
+                    null}
+                <button onClick={this.handleChange}>
+                    {this.state.isLogin ? 'Скрыть' : 'Показать'}
+                </button>
+            </div>
+
+        )
+    }
+}
+
+export default UserInfo; 
